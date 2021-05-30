25dd11f79e6ecea1dfd8583fa1ce5e7b7ced347f
ryanshu <876324872@qq.com>
2021-05-31 02:09:42 +0800

    Refactor:get user info

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -3,14 +3,16 @@ const api = require('./utils/api.js')
 
 App({
   globalData:{
-    baseUrl:'https://api.ddoudou.xyz/api/',
+    // baseUrl:'https://api.notspr.com/api/',
+    baseUrl:'http://127.0.0.1:8000/api/',
     StatusBar:'',
     windowHeight:'',
     user:{
-      openid:'',
+      openid:'11',
       ava:'',
       fakeName:''
-    }
+    },
+    userInfo: {},
   },
 
   func:{
@@ -20,7 +22,6 @@ App({
   onLaunch: function () {
     wx.getSystemInfo({
       success: e => {
-        console.log(e);
         this.globalData.StatusBar = e.statusBarHeight;
         let custom = wx.getMenuButtonBoundingClientRect();
         this.globalData.Custom = custom;
@@ -37,7 +38,7 @@ App({
         //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
         //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
         //   如不填则使用默认环境（第一个创建的环境）
-        env: 'pangpangbot-xpx3g',
+        env: 'cloud1-7ghl2kdt625331d2',
         traceUser: true,
       })
     }
@@ -46,17 +47,24 @@ App({
       name:'login',
       success:res=>{
         this.globalData.user.openid = res.result.openid
+        this.globalData.user.ava = res.result.ava
+        // this.getUserInfo()
       }
     })
-    
+
+
+  },
+
+  getUserInfo() {
     wx.request({
-      url: this.globalData.baseUrl+'users/1231312',
+      url: this.globalData.baseUrl + `users/${this.globalData.user.openid}/`,
       method:'GET',
       success:res=>{
         this.globalData.user.fakeName = res.data.fakename;
+        console.log(res)
       },
       fail:res=>{
-
+        console.log(res)
       }
     })
   }
