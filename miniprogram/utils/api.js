f34919d6d39e1b1bd60312c97ae35dc5cfe35fc1
ryanshu <876324872@qq.com>
2021-05-31 02:09:08 +0800

    Refactor:common api

--- miniprogram/utils/api.js
+++ miniprogram/utils/api.js
@@ -1,28 +1,29 @@
-const baseUrl = 'https://api.ddoudou.xyz/api/'
-const apiList={
-  moods:'moods/',
-  comments:'comments/',
-  users:'users/'
+// const baseUrl = 'https://api.notspr.com/api/'
+const baseUrl = 'http://127.0.0.1:8000/api/'
+const apiList = {
+    moods: 'moods/',
+    comments: 'comments/',
+    users: 'users/'
 }
 
-function getData(apiName,data,type,func){
+function getData(apiName, data, type, func) {
     wx.request({
-      url: baseUrl+apiList[apiName],
-      data:data,
-      method:type,
-      success:res=>{
-        return typeof func== "function" && func(res.data)
-      },
-      fail:function(){
-        reject:('网络错误')
-        wx.showToast({
-          icon:'none',
-          title: '网络错误',
-        })
-      }
+        url: baseUrl + apiList[apiName],
+        data: data,
+        method: type,
+        success: res => {
+            return typeof func == "function" && func(res.data)
+        },
+        fail: function () {
+            reject:('网络错误')
+            wx.showToast({
+                icon: 'none',
+                title: '网络错误',
+            })
+        }
     })
 }
 
-module.exports={
-  ppApi:getData
+module.exports = {
+    ppApi: getData
 }
